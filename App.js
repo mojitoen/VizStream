9373fdbed5fe05fa6f23bbf233240bb8a6de0859
Marcus Manuel Feliciano <marcus@feliciano.eu>
2023-06-01 09:21:01 +0200

    Knapp, installert obs-websocket-js i package.json
    +Knapp for å teste med backend
    +La til obs-websocket-js

--- App.js
+++ App.js
@@ -1,10 +1,12 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Button } from 'react-native';
+
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text>WOW TEST VERY COOL</Text>
+      <Button title='testknapp3000'></Button>
       <StatusBar style="auto" />
     </View>
   );
