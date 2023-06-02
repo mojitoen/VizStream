cefdb7c8be1347dd31505895f7a1182ea21aa554
mojitoen <marcus@feliciano.eu>
2023-06-02 14:27:34 +0200

    Integrering
    +Begynt på integreringen av Front-end og back-end.
    
    Har tatt deler av js filer frontend har gitt oss, og implementert dem i App.js
    
    +Ryddet i imports
    +Kommentert kode
    +Byttet buttonlabels
    
    Scenene er for øyeblikket hard-coded inn, må skrive en funksjon for å hente listen over alle scenene og sette de inn i buttonLabels.

--- App.js
+++ App.js
@@ -1,7 +1,7 @@
 import { StatusBar } from 'expo-status-bar';
-import { SafeAreaView } from 'react-native-web';
 import { useEffect, useState } from 'react';
-import { StyleSheet, Text, View, TouchableOpacity, TextInput, Image} from 'react-native';
+import { View, StyleSheet, Text, TouchableOpacity, TextInput, Image, SafeAreaView} from 'react-native';
+import React from 'react';
 import OBSWebSocket from 'obs-websocket-js';
 
 
@@ -11,10 +11,19 @@ const [ipAddress, setIpAddress] = useState('');
 
 //Temp usestate som henter verdien til inputText onchange. Dette gjør at ikke useEffecten kjører mange ganger mens bruker skriver IP.
 const [inputText, setInputText] = useState('');
-
+//State var for checking whether or not we are connected
 const [connectedStatus, setConnectedStatus] = useState(false)
 
+//OBS Tilkobling satt i en state, slik at tilkoblingen opprettholder seg
 const [obs, setObs] = useState(new OBSWebSocket());
+
+//Midlertidig satt knappe-verdier. Disse kan heller hentes fra en JSON-fil slik at tittelen til knappen bestemmer hva knappen gjør
+const buttonLabels = [
+  ['Set Scene to Playing Game', 'Start Stream'],
+  ['Set Scene to Be Right Back', 'Button 4'],
+  ['Set Scene to TalkToChat', 'Start Recording'],
+  ['Button 7', 'Button 8']
+];
 
   useEffect(() => {
     // Kobler seg til OBS og gir beskjed om at den er tilkoblet
@@ -45,7 +54,6 @@ const [obs, setObs] = useState(new OBSWebSocket());
   const getSceneBtn = async () => {
     try {
       if (obs) {
-        //await setScene(obs, 'KulScene2');
         console.log(await getScene(obs));
         return await getScene(obs);
       } else {
@@ -56,40 +64,95 @@ const [obs, setObs] = useState(new OBSWebSocket());
     }
   };
 
+  //En asynkron-funksjon som fungerer som bro til en synk knapp
    const getScene = async (obs) => {
     return await obs.call('GetCurrentProgramScene');
   };
 
   //Når bruker klikker "connect" knappen, så settes ipAddress til verdien av inputText sånn at connection funksjonen kjøres.
-  const handleButtonClick = () => {
+  const handleButtonClickIP = () => {
     setIpAddress(inputText)
   };
 
+  //Funksjon for å trykke på knapper
+  const handleButtonClick = (buttonTitle) => {
+    //Midlertidig hardcode
+    //Her er det kanskje kurant å sette opp så mange knappe-funksjoner som mulig, så kan vi definere her hva en individuell knapp gjør
+    if(buttonTitle == "Set Scene to Playing Game") {
+      setScene(obs, "Playing Game")
+    }
+
+    if(buttonTitle == "Set Scene to Be Right Back") {
+      setScene(obs, "Be Right Back")
+    }
+
+    if(buttonTitle == "Set Scene to TalkToChat") {
+      setScene(obs, "TalkToChat")
+    }
+
+    if(buttonTitle == "Start Stream") {
+      obs.call('StartStream');
+    }
+  }
+
+  //Async funksjon for å overføre til synk funksjon
+  const setScene = async (obs, scene) => {
+    await obs.call('SetCurrentProgramScene', { sceneName: scene });
+  };
+
+//Synnes connection-frontend returneres hvis useState connectedStatus ikke stemmer
   if(!connectedStatus) {
     return (
-    
-      <View style={styles.container}>
-        <Image style={styles.Image} source={require('./assets/vizrt-logo-front.png')}></Image>
-        <Text style={styles.Text}>Connect to OBS</Text>
+      <View style={stylesconnect.container}>
+        <Image style={stylesconnect.Image} source={require('./assets/vizrt-logo-front.png')}></Image>
+        <Text style={stylesconnect.Text}>Connect to OBS</Text>
         <StatusBar style="auto" />
-        <TextInput style={styles.TextInput} value={inputText} onChangeText={setInputText} placeholder="IP-address"/>
-        <TouchableOpacity style={styles.ConnectBtn} onPress={handleButtonClick}>  
-        <Text style={styles.btnText}>Connect</Text>
+        <TextInput style={stylesconnect.TextInput} value={inputText} onChangeText={setInputText} placeholder="IP-address"/>
+        <TouchableOpacity style={stylesconnect.ConnectBtn} onPress={handleButtonClickIP}>  
+        <Text style={stylesconnect.btnText}>Connect</Text>
         </TouchableOpacity>
       </View>
     );
+    //Hvis vi har koblet oss til, så re-renderer vi til Matias sin Knapp-frontend.
   } else if (connectedStatus) {
     return (
-      <>
-        <TouchableOpacity onPress={getSceneBtn}>
-          <Text>Get Scene List</Text>
-        </TouchableOpacity>
-      </>
+      <SafeAreaView style={styles.container}>
+      {/* Top Navigation */}
+      <View style={styles.navigation}>
+        {/* Add your components for top navigation */}
+      </View>
+
+      {/* Grid */}
+      <View style={styles.gridContainer}>
+        {buttonLabels.map((row, rowIndex) => (
+          <View key={rowIndex} style={styles.row}>
+            {row.map((title, cellIndex) => (
+
+              <TouchableOpacity
+                key={cellIndex}
+                style={styles.cell}
+                onPress={() => handleButtonClick(title)}>
+
+                <Text 
+                    style={styles.buttonText}>{title}
+                </Text>
+              </TouchableOpacity>
+            ))}
+          </View>
+        ))}
+      </View>
+
+      {/* Bottom Navigation */}
+      <View style={styles.navigation}>
+        {/* Add your components for bottom navigation */}
+      </View>
+    </SafeAreaView>
     )
   }
 }
 
-const styles = StyleSheet.create({
+//Styliseringen for Connection-siden. 
+const stylesconnect = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#1C3640',
@@ -120,6 +183,42 @@ const styles = StyleSheet.create({
     width: 300,
   }
 
+});
+
+
+//Stylisation for knapp-viewen
+const styles = StyleSheet.create({
+ 
+  container: {
+  backgroundColor:'#1C3640',
+  flex: 1,
+},
+navigation: {
+  height: 50,
+  backgroundColor: '#1C3640',
+  justifyContent: 'center',
+  alignItems: 'center',
+},
+gridContainer: {
+  flex: 1,
+  paddingLeft: 40,
+  paddingRight: 40,
+},
+row: {
+  flex: 1,
+  flexDirection: 'row',
+},
+cell: {
+  flex: 1,
+  justifyContent: 'center',
+  alignItems: 'center',
+  borderWidth: 1,
+  borderColor: 'black',
+},  
+  buttonText: {
+  color: 'white',
+  fontSize: 16,
+},
 });
 
 export default App; 
