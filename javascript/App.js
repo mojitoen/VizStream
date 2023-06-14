83b262e6c063a2473123e938c76622c6a3601b85
MatiasMogensen <89384310+MatiasMogensen@users.noreply.github.com>
2023-06-14 13:57:36 +0200

    Failed login text Skal pushe til Main i morgen når jeg har Marcus sin server oppe slik at jeg ser at alt er i orden

--- javascript/App.js
+++ javascript/App.js
@@ -1,10 +1,28 @@
 import { StatusBar } from 'expo-status-bar';
 import { SafeAreaView } from 'react-native-web';
 import { StyleSheet, Text, View, TouchableOpacity, TextInput, Image} from 'react-native';
+import { useState } from 'react';
 
 
 
 function App() {
+
+// 11 til 24 er alt vi trenger til å få til error message
+
+  const [opacity, setOpacity] = useState(0)
+  
+  const loginFailed=() =>{
+    setOpacity(opacity === 0 ? 1 : 0)
+  };
+
+  const failedStyle = {
+    opacity: opacity,
+    color: 'red',
+    fontSize: 20,
+    padding: 10,
+
+  }
+
   return (
     
     <View style={styles.container}>
@@ -12,7 +30,12 @@ function App() {
       <Text style={styles.Text}>Connect to OBS</Text>
       <StatusBar style="auto" />
       <TextInput style={styles.TextInput} placeholder="IP-adress"/>
-      <TouchableOpacity style={styles.ConnectBtn}>  
+
+      {/* Bruk style og onPress i main for å få til error message */}
+
+      <Text style={failedStyle}>Failed to log in. Please try again</Text>
+      <TouchableOpacity style={styles.ConnectBtn} onPress={loginFailed}>  
+
       <Text style={styles.btnText}>Connect</Text>
       </TouchableOpacity>
     </View>
@@ -48,6 +71,13 @@ const styles = StyleSheet.create({
   Image:{
     height: 150, 
     width: 300,
+  },
+  loginFailed:{
+    color: 'red',
+    fontSize: 20,
+    padding: 10,
+    opacity: 0
+
   }
 
 });
